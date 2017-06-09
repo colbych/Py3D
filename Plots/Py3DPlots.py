fc1ccd3f87a3cfac9d780ff89084d601372e5e49
Squid49134 <ransom@Udel.edu>
2017-06-09 01:47:48 -0400

    fixed subsampling

--- Plots/Py3DPlots.py
+++ Plots/Py3DPlots.py
@@ -1,34 +1,34 @@
 import numpy as np
-import matplotlib.pyplot as sp
+import matplotlib.pyplot as plt
 from Py3D.sub import load_movie
 
 def TwoD_SubSample64(Par_Orig):
     print("Subsampling....")
     Par_Sub = np.empty([1024, 1024])
-    for i in range (1,1024):
-        for n in range(1,1024):
-            Par_Sub[i,n] = Par_Orig[((i*8)-1),((n*8)-1)]
+    for i in range (0,1024):
+        for n in range(0,1024):
+            Par_Sub[i,n] = Par_Orig[i*8,n*8]
     return Par_Sub
     
 def OneD_SubSample64(Par_Orig):
     print("Subsampling....")
     Par_Sub = np.empty([1024])
-    for i in range (1,1024):
-        Par_Sub[i] = Par_Orig[((i*8)-1)]
+    for i in range (0,1024):
+        Par_Sub[i] = Par_Orig[i*8]
     return Par_Sub
 
-def SubSampleDictionary(dict):
-    New_dict = {'bx': TwoD_SubSample64(dict['bx']),'by': TwoD_SubSample64(dict['by']),'bz': TwoD_SubSample64(dict['bz']),
-    'ex': TwoD_SubSample64(dict['ex']),'ey': TwoD_SubSample64(dict['ey']),'ez': TwoD_SubSample64(dict['ez']),
-    'jx': TwoD_SubSample64(dict['jx']),'jy': TwoD_SubSample64(dict['jy']),'jz': TwoD_SubSample64(dict['jz']),
-    'ni': TwoD_SubSample64(dict['ni']),'ne': TwoD_SubSample64(dict['ne']),'rho': TwoD_SubSample64(dict['rho']),
-    'jex': TwoD_SubSample64(dict['jex']),'jey': TwoD_SubSample64(dict['jey']),'jez': TwoD_SubSample64(dict['jez']),
-    'jix': TwoD_SubSample64(dict['jix']),'jiy': TwoD_SubSample64(dict['jiy']),'jiz': TwoD_SubSample64(dict['jiz']),
-    'pexx': TwoD_SubSample64(dict['pexx']),'peyy': TwoD_SubSample64(dict['peyy']),'pezz': TwoD_SubSample64(dict['pezz']),
-    'pixx': TwoD_SubSample64(dict['pixx']),'piyy': TwoD_SubSample64(dict['piyy']),'pizz': TwoD_SubSample64(dict['pizz']),
-    'pexy': TwoD_SubSample64(dict['pexy']),'peyz': TwoD_SubSample64(dict['peyz']),'pexz': TwoD_SubSample64(dict['pexz']),
-    'pixy': TwoD_SubSample64(dict['pixy']),'piyz': TwoD_SubSample64(dict['piyz']),'pixz': TwoD_SubSample64(dict['pixz']),
-    'xx': OneD_SubSample64(dict['xx']),'yy': OneD_SubSample64(dict['yy'])}
+def SubSampleDictionary(Q):
+    New_dict = {'bx': TwoD_SubSample64(Q['bx']),'by': TwoD_SubSample64(Q['by']),'bz': TwoD_SubSample64(Q['bz']),
+    'ex': TwoD_SubSample64(Q['ex']),'ey': TwoD_SubSample64(Q['ey']),'ez': TwoD_SubSample64(Q['ez']),
+    'jx': TwoD_SubSample64(Q['jx']),'jy': TwoD_SubSample64(Q['jy']),'jz': TwoD_SubSample64(Q['jz']),
+    'ni': TwoD_SubSample64(Q['ni']),'ne': TwoD_SubSample64(Q['ne']),'rho': TwoD_SubSample64(Q['rho']),
+    'jex': TwoD_SubSample64(Q['jex']),'jey': TwoD_SubSample64(Q['jey']),'jez': TwoD_SubSample64(Q['jez']),
+    'jix': TwoD_SubSample64(Q['jix']),'jiy': TwoD_SubSample64(Q['jiy']),'jiz': TwoD_SubSample64(Q['jiz']),
+    'pexx': TwoD_SubSample64(Q['pexx']),'peyy': TwoD_SubSample64(Q['peyy']),'pezz': TwoD_SubSample64(Q['pezz']),
+    'pixx': TwoD_SubSample64(Q['pixx']),'piyy': TwoD_SubSample64(Q['piyy']),'pizz': TwoD_SubSample64(Q['pizz']),
+    'pexy': TwoD_SubSample64(Q['pexy']),'peyz': TwoD_SubSample64(Q['peyz']),'pexz': TwoD_SubSample64(Q['pexz']),
+    'pixy': TwoD_SubSample64(Q['pixy']),'piyz': TwoD_SubSample64(Q['piyz']),'pixz': TwoD_SubSample64(Q['pixz']),
+    'xx': OneD_SubSample64(Q['xx']),'yy': OneD_SubSample64(Q['yy'])}
     return New_dict
 
 
@@ -40,16 +40,14 @@ def P_1_6_Plots():
 #-----------------------------------------------
 
     print("Plotting Page 1...")
-
-    Page1 = sp.figure(1)
+    Page1 = plt.figure(1)
+    Page1.set_size_inches(8.5,11, forward = True)
     Page1.subplots_adjust(hspace = .5)
-    # resize not working???
-    Page1.set_size_inches(8.5,11)
 
     sp11 = Page1.add_subplot(321)
     sp11.pcolormesh(X,Y,d['bx'])
     sp11.locator_params(nbins = 6)
-    sp11.set_title("$B_x$")
+    sp11.set_title("$B_x$", fontsize=20)
     sp11.set_aspect(1)
     sp11.set_xlim([0,102.5])
     sp11.set_ylim([0,102.5])    
@@ -57,7 +55,7 @@ def P_1_6_Plots():
     sp12 = Page1.add_subplot(322)
     sp12.pcolormesh(X,Y,d['by'])
     sp12.locator_params(nbins = 6)
-    sp12.set_title("$B_y$")
+    sp12.set_title("$B_y$", fontsize=20)
     sp12.set_aspect(1)
     sp12.set_xlim([0,102.5])
     sp12.set_ylim([0,102.5])
@@ -65,7 +63,7 @@ def P_1_6_Plots():
     sp13 = Page1.add_subplot(323)
     sp13.pcolormesh(X,Y,d['bz'])
     sp13.locator_params(nbins = 6)
-    sp13.set_title("$B_z$")
+    sp13.set_title("$B_z$", fontsize=20)
     sp13.set_aspect(1)
     sp13.set_xlim([0,102.5])
     sp13.set_ylim([0,102.5])
@@ -74,7 +72,7 @@ def P_1_6_Plots():
     sp14 = Page1.add_subplot(324)
     sp14.pcolormesh(X,Y,Mb)
     sp14.locator_params(nbins = 6)
-    sp14.set_title("$\mid B\mid$")
+    sp14.set_title("$\mid B\mid$", fontsize=20)
     sp14.set_aspect(1)
     sp14.set_xlim([0,102.5])
     sp14.set_ylim([0,102.5])
@@ -82,7 +80,7 @@ def P_1_6_Plots():
     sp15 = Page1.add_subplot(325)
     sp15.pcolormesh(X,Y,d['ni'])
     sp15.locator_params(nbins = 6)
-    sp15.set_title("$N_i$")
+    sp15.set_title("$N_i$", fontsize=20)
     sp15.set_aspect(1)
     sp15.set_xlim([0,102.5])
     sp15.set_ylim([0,102.5])
@@ -90,7 +88,7 @@ def P_1_6_Plots():
     sp16 = Page1.add_subplot(326)
     sp16.pcolormesh(X,Y,d['ne'])
     sp16.locator_params(nbins = 6)
-    sp16.set_title("$N_e$")
+    sp16.set_title("$N_e$", fontsize=20)
     sp16.set_aspect(1)
     sp16.set_xlim([0,102.5])
     sp16.set_ylim([0,102.5])
@@ -108,14 +106,14 @@ def P_1_6_Plots():
 
     print("Plotting Page 2...")
 
-    Page2 = sp.figure(2)
+    Page2 = plt.figure(2)
     Page2.subplots_adjust(hspace = .5)
-    Page2.set_size_inches(8.5,11)
+    Page2.set_size_inches(8.5,11, forward = True)
 
     sp21 = Page2.add_subplot(321)
     sp21.pcolormesh(X,Y,d['ex'])
     sp21.locator_params(nbins = 6)
-    sp21.set_title("$E_x$")
+    sp21.set_title("$E_x$", fontsize=20)
     sp21.set_aspect(1)
     sp21.set_xlim([0,102.5])
     sp21.set_ylim([0,102.5])
@@ -123,7 +121,7 @@ def P_1_6_Plots():
     sp22 = Page2.add_subplot(322)
     sp22.pcolormesh(X,Y,d['ey'])
     sp22.locator_params(nbins = 6)
-    sp22.set_title("$E_y$")
+    sp22.set_title("$E_y$", fontsize=20)
     sp22.set_aspect(1)
     sp22.set_xlim([0,102.5])
     sp22.set_ylim([0,102.5])
@@ -131,7 +129,7 @@ def P_1_6_Plots():
     sp23 = Page2.add_subplot(323)
     sp23.pcolormesh(X,Y,d['ez'])
     sp23.locator_params(nbins = 6)
-    sp23.set_title("$E_z$")
+    sp23.set_title("$E_z$", fontsize=20)
     sp23.set_aspect(1)
     sp23.set_xlim([0,102.5])
     sp23.set_ylim([0,102.5])
@@ -140,7 +138,7 @@ def P_1_6_Plots():
     sp24 = Page2.add_subplot(324)
     sp24.pcolormesh(X,Y,Me)
     sp24.locator_params(nbins = 6)
-    sp24.set_title("$\mid E\mid$")
+    sp24.set_title("$\mid E\mid$", fontsize=20)
     sp24.set_aspect(1)
     sp24.set_xlim([0,102.5])
     sp24.set_ylim([0,102.5])
@@ -149,7 +147,7 @@ def P_1_6_Plots():
     sp25 = Page2.add_subplot(325)
     sp25.pcolormesh(X,Y,EdB)
     sp25.locator_params(nbins = 6)
-    sp25.set_title("$E\cdot B$")
+    sp25.set_title("$E\cdot B$", fontsize=20)
     sp25.set_aspect(1)
     sp25.set_xlim([0,102.5])
     sp25.set_ylim([0,102.5])
@@ -157,7 +155,7 @@ def P_1_6_Plots():
     sp26 = Page2.add_subplot(326)
     sp26.pcolormesh(X,Y,d['rho'])
     sp26.locator_params(nbins = 6)
-    sp26.set_title("$P$")
+    sp26.set_title("$P$", fontsize=20)
     sp26.set_aspect(1)
     sp26.set_xlim([0,102.5])
     sp26.set_ylim([0,102.5])
@@ -172,14 +170,14 @@ def P_1_6_Plots():
 
     print("Plotting Page 3...")
 
-    Page3 = sp.figure(3)
+    Page3 = plt.figure(3)
     Page3.subplots_adjust(hspace = .5)
-    Page3.set_size_inches(8.5,11)
+    Page3.set_size_inches(8.5,11, forward = True)
 
     sp31 = Page3.add_subplot(321)
     sp31.pcolormesh(X,Y,d['jx'])
     sp31.locator_params(nbins = 6)
-    sp31.set_title("$J_x$")
+    sp31.set_title("$J_x$", fontsize=20)
     sp31.set_aspect(1)
     sp31.set_xlim([0,102.5])
     sp31.set_ylim([0,102.5])
@@ -187,7 +185,7 @@ def P_1_6_Plots():
     sp32 = Page3.add_subplot(322)
     sp32.pcolormesh(X,Y,d['jy'])
     sp32.locator_params(nbins = 6)
-    sp32.set_title("$J_y$")
+    sp32.set_title("$J_y$", fontsize=20)
     sp32.set_aspect(1)
     sp32.set_xlim([0,102.5])
     sp32.set_ylim([0,102.5])
@@ -195,7 +193,7 @@ def P_1_6_Plots():
     sp33 = Page3.add_subplot(323)
     sp33.pcolormesh(X,Y,d['jz'])
     sp33.locator_params(nbins = 6)
-    sp33.set_title("$J_z$")
+    sp33.set_title("$J_z$", fontsize=20)
     sp33.set_aspect(1)
     sp33.set_xlim([0,102.5])
     sp33.set_ylim([0,102.5])
@@ -203,7 +201,7 @@ def P_1_6_Plots():
     sp34 = Page3.add_subplot(324)
     sp34.pcolormesh(X,Y,d['jix'])
     sp34.locator_params(nbins = 6)
-    sp34.set_title("$J_{ix}$")
+    sp34.set_title("$J_{ix}$", fontsize=20)
     sp34.set_aspect(1)
     sp34.set_xlim([0,102.5])
     sp34.set_ylim([0,102.5])
@@ -211,7 +209,7 @@ def P_1_6_Plots():
     sp35 = Page3.add_subplot(325)
     sp35.pcolormesh(X,Y,d['jiy'])
     sp35.locator_params(nbins = 6)
-    sp35.set_title("$J_{iy}$")
+    sp35.set_title("$J_{iy}$", fontsize=20)
     sp35.set_aspect(1)
     sp35.set_xlim([0,102.5])
     sp35.set_ylim([0,102.5])
@@ -219,7 +217,7 @@ def P_1_6_Plots():
     sp36 = Page3.add_subplot(326)
     sp36.pcolormesh(X,Y,d['jiz'])
     sp36.locator_params(nbins = 6)
-    sp36.set_title("$J_{iz}$")
+    sp36.set_title("$J_{iz}$", fontsize=20)
     sp36.set_aspect(1)
     sp36.set_xlim([0,102.5])
     sp36.set_ylim([0,102.5])
@@ -234,14 +232,14 @@ def P_1_6_Plots():
 
     print("Plotting Page 4...")
 
-    Page4 = sp.figure(4)
+    Page4 = plt.figure(4)
     Page4.subplots_adjust(hspace = .5)
-    Page4.set_size_inches(8.5,11)
+    Page4.set_size_inches(8.5,11, forward = True)
 
     sp41 = Page4.add_subplot(321)
     sp41.pcolormesh(X,Y,d['jex'])
     sp41.locator_params(nbins = 6)
-    sp41.set_title("$J_{ex}$")
+    sp41.set_title("$J_{ex}$", fontsize=20)
     sp41.set_aspect(1)
     sp41.set_xlim([0,102.5])
     sp41.set_ylim([0,102.5])
@@ -249,7 +247,7 @@ def P_1_6_Plots():
     sp42 = Page4.add_subplot(322)
     sp42.pcolormesh(X,Y,d['jey'])
     sp42.locator_params(nbins = 6)
-    sp42.set_title("$J_{ey}$")
+    sp42.set_title("$J_{ey}$", fontsize=20)
     sp42.set_aspect(1)
     sp42.set_xlim([0,102.5])
     sp42.set_ylim([0,102.5])
@@ -257,7 +255,7 @@ def P_1_6_Plots():
     sp43 = Page4.add_subplot(323)
     sp43.pcolormesh(X,Y,d['jez'])
     sp43.locator_params(nbins = 6)
-    sp43.set_title("$J_{ez}$")
+    sp43.set_title("$J_{ez}$", fontsize=20)
     sp43.set_aspect(1)
     sp43.set_xlim([0,102.5])
     sp43.set_ylim([0,102.5])
@@ -266,7 +264,7 @@ def P_1_6_Plots():
     sp44 = Page4.add_subplot(324)
     sp44.pcolormesh(X,Y,Vex)
     sp44.locator_params(nbins = 6)
-    sp44.set_title("$V_{ex}$")
+    sp44.set_title("$V_{ex}$", fontsize=20)
     sp44.set_aspect(1)
     sp44.set_xlim([0,102.5])
     sp44.set_ylim([0,102.5])
@@ -275,7 +273,7 @@ def P_1_6_Plots():
     sp45 = Page4.add_subplot(325)
     sp45.pcolormesh(X,Y,Vey)
     sp45.locator_params(nbins = 6)
-    sp45.set_title("$V_{ey}$")
+    sp45.set_title("$V_{ey}$", fontsize=20)
     sp45.set_aspect(1)
     sp45.set_xlim([0,102.5])
     sp45.set_ylim([0,102.5])
@@ -284,7 +282,7 @@ def P_1_6_Plots():
     sp46 = Page4.add_subplot(326)
     sp46.pcolormesh(X,Y,Vez)
     sp46.locator_params(nbins = 6)
-    sp46.set_title("$V_{ez}$")
+    sp46.set_title("$V_{ez}$", fontsize=20)
     sp46.set_aspect(1)
     sp46.set_xlim([0,102.5])
     sp46.set_ylim([0,102.5])
@@ -299,15 +297,15 @@ def P_1_6_Plots():
 
     print("Plotting Page 5...")
 
-    Page5 = sp.figure(5)
+    Page5 = plt.figure(5)
     Page5.subplots_adjust(hspace = .5)
-    Page5.set_size_inches(8.5,11)
+    Page5.set_size_inches(8.5,11, forward = True)
 
     Vix = d['jix']/d['ni']
     sp51 = Page5.add_subplot(321)
     sp51.pcolormesh(X,Y,Vix)
     sp51.locator_params(nbins = 6)
-    sp51.set_title("$V_{ix}$")
+    sp51.set_title("$V_{ix}$", fontsize=20)
     sp51.set_aspect(1)
     sp51.set_xlim([0,102.5])
     sp51.set_ylim([0,102.5])
@@ -316,7 +314,7 @@ def P_1_6_Plots():
     sp52 = Page5.add_subplot(322)
     sp52.pcolormesh(X,Y,Viy)
     sp52.locator_params(nbins = 6)
-    sp52.set_title("$V_{iy}$")
+    sp52.set_title("$V_{iy}$", fontsize=20)
     sp52.set_aspect(1)
     sp52.set_xlim([0,102.5])
     sp52.set_ylim([0,102.5])
@@ -325,7 +323,7 @@ def P_1_6_Plots():
     sp53 = Page5.add_subplot(323)
     sp53.pcolormesh(X,Y,Viz)
     sp53.locator_params(nbins = 6)
-    sp53.set_title("$V_{iz}$")
+    sp53.set_title("$V_{iz}$", fontsize=20)
     sp53.set_aspect(1)
     sp53.set_xlim([0,102.5])
     sp53.set_ylim([0,102.5])
@@ -334,7 +332,7 @@ def P_1_6_Plots():
     sp54 = Page5.add_subplot(324)
     sp54.pcolormesh(X,Y,Tixx)
     sp54.locator_params(nbins = 6)
-    sp54.set_title("$T_{ixx}$")
+    sp54.set_title("$T_{ixx}$", fontsize=20)
     sp54.set_aspect(1)
     sp54.set_xlim([0,102.5])
     sp54.set_ylim([0,102.5])
@@ -343,7 +341,7 @@ def P_1_6_Plots():
     sp55 = Page5.add_subplot(325)
     sp55.pcolormesh(X,Y,Tiyy)
     sp55.locator_params(nbins = 6)
-    sp55.set_title("$T_{iyy}$")
+    sp55.set_title("$T_{iyy}$", fontsize=20)
     sp55.set_aspect(1)
     sp55.set_xlim([0,102.5])
     sp55.set_ylim([0,102.5])
@@ -352,7 +350,7 @@ def P_1_6_Plots():
     sp56 = Page5.add_subplot(326)
     sp56.pcolormesh(X,Y,Tizz)
     sp56.locator_params(nbins = 6)
-    sp56.set_title("$T_{izz}$")
+    sp56.set_title("$T_{izz}$", fontsize=20)
     sp56.set_aspect(1)
     sp56.set_xlim([0,102.5])
     sp56.set_ylim([0,102.5])
@@ -367,15 +365,15 @@ def P_1_6_Plots():
 
     print("Plotting Page 6...")
 
-    Page6 = sp.figure(6)
+    Page6 = plt.figure(6)
     Page6.subplots_adjust(hspace = .5)
-    Page6.set_size_inches(8.5,11)
+    Page6.set_size_inches(8.5,11, forward = True)
 
     Texx = d['pexx']/d['ne']
     sp61 = Page6.add_subplot(321)
     sp61.pcolormesh(X,Y,Texx)
     sp61.locator_params(nbins = 6)
-    sp61.set_title("$T_{exx}$")
+    sp61.set_title("$T_{exx}$", fontsize=20)
     sp61.set_aspect(1)
     sp61.set_xlim([0,102.5])
     sp61.set_ylim([0,102.5])
@@ -384,7 +382,7 @@ def P_1_6_Plots():
     sp62 = Page6.add_subplot(322)
     sp62.pcolormesh(X,Y,Teyy)
     sp62.locator_params(nbins = 6)
-    sp62.set_title("$T_{eyy}$")
+    sp62.set_title("$T_{eyy}$", fontsize=20)
     sp62.set_aspect(1)
     sp62.set_xlim([0,102.5])
     sp62.set_ylim([0,102.5])
@@ -393,7 +391,7 @@ def P_1_6_Plots():
     sp63 = Page6.add_subplot(323)
     sp63.pcolormesh(X,Y,Tezz)
     sp63.locator_params(nbins = 6)
-    sp63.set_title("$T_{ezz}$")
+    sp63.set_title("$T_{ezz}$", fontsize=20)
     sp63.set_aspect(1)
     sp63.set_xlim([0,102.5])
     sp63.set_ylim([0,102.5])
@@ -402,7 +400,7 @@ def P_1_6_Plots():
     sp64 = Page6.add_subplot(324)
     sp64.pcolormesh(X,Y,Texy)
     sp64.locator_params(nbins = 6)
-    sp64.set_title("$T_{exy}$")
+    sp64.set_title("$T_{exy}$", fontsize=20)
     sp64.set_aspect(1)
     sp64.set_xlim([0,102.5])
     sp64.set_ylim([0,102.5])
@@ -411,7 +409,7 @@ def P_1_6_Plots():
     sp65 = Page6.add_subplot(325)
     sp65.pcolormesh(X,Y,Texz)
     sp65.locator_params(nbins = 6)
-    sp65.set_title("$T_{exz}$")
+    sp65.set_title("$T_{exz}$", fontsize=20)
     sp65.set_aspect(1)
     sp65.set_xlim([0,102.5])
     sp65.set_ylim([0,102.5])
@@ -420,7 +418,7 @@ def P_1_6_Plots():
     sp66 = Page6.add_subplot(326)
     sp66.pcolormesh(X,Y,Teyz)
     sp66.locator_params(nbins = 6)
-    sp66.set_title("$T_{eyz}$")
+    sp66.set_title("$T_{eyz}$", fontsize=20)
     sp66.set_aspect(1)
     sp66.set_xlim([0,102.5])
     sp66.set_ylim([0,102.5])
