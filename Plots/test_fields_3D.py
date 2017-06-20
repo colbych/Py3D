b65c5bb72ca6011aecadc7d234d392b27ddc6954
Colby Haggerty <colbych@udel.edu>
2017-06-20 09:36:46 -0400

    Fixed up 3D test fields

--- Plots/test_fields_3D.py
+++ Plots/test_fields_3D.py
@@ -16,14 +16,29 @@ rr = np.sqrt((xx - lx/2.)**2 + (yy - ly/2.)**2)
 
 psiM = np.exp(-rr**2/8.)
 
-Bx = (roll(psiM,-1,axis=1) - roll(psiM,1,axis=1))/2./dx
-By = -(roll(psiM,-1,axis=0) - roll(psiM,1,axis=0))/2./dx
-Bm = np.sqrt(Bx**2 + By**2)
+Bx = (np.roll(psiM,-1,axis=1) - np.roll(psiM,1,axis=1))/2./dx
+By = -(np.roll(psiM,-1,axis=0) - np.roll(psiM,1,axis=0))/2./dx
+#Bz = psiM
+Bz = 0.*zz + 1.
 
-levels = linspace(0,1,20)
+Bm = np.sqrt(Bx**2 + By**2 + Bz**2)
+
+def dif(i,f):
+    return (np.roll(f,-1,axis=i) - np.roll(f,1,axis=i))/2./dx
+
+# You can comment the divergence and curl parts out if you want.
+
+# Calculate the Divergence, make sure it is 0
+DB = dif(0, Bx) + dif(1, By) + dif(2, Bz)
+
+# Calculate the Curl, this gives J
+CBx = dif(1, Bz) - dif(2, By) 
+CBy = dif(2, Bx) - dif(0, Bz) 
+CBz = dif(0, By) - dif(1, Bx) 
+
 plt.clf()
-plt.contour(x, x, psiM[:,:,0], levels=[.5], colors='k')
-#plt.pcolormesh(x, x, psiM)
-#plt.pcolormesh(x, x, Bm)
+# This is no longer the correct field line in the strict sence
+plt.contour(x, y, psiM[:,:,0], levels=[.125,.25,.5,1.,2.], colors='k')
+plt.pcolormesh(x, y, Bm[:,:,0])
 plt.gca().set_aspect('equal')
-
+plt.show()
